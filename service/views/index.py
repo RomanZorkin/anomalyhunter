c1cc56713f47cf47fa6eb04eafe653e67344aa1e
RomanZorkin <zorkin.roman@gmail.com>
2022-08-05 22:42:02 +0700

    feature: create upload sceleton

--- service/views/index.py
+++ service/views/index.py
@@ -1,8 +1,16 @@
 from flask import Blueprint, render_template
 
+from service.forms.upload import FileForm
+
 view = Blueprint('index', __name__)
 
 
 @view.route('/', methods=['GET', 'POST'])
 def index():
-    return render_template('index.html')
+    form = FileForm(meta={'csrf': False})
+    title = 'Поиск аномалий'
+    return render_template(
+        'index.html',
+        page_title=title,
+        form=form,
+    )
