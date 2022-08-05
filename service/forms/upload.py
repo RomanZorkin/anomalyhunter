b876f48a8e4107866b227adbb041b4106161bf78
RomanZorkin <zorkin.roman@gmail.com>
2022-08-05 23:49:03 +0700

    feature: implemetn upload simple  service

--- service/forms/upload.py
+++ service/forms/upload.py
@@ -1,16 +1,6 @@
 from flask_wtf import FlaskForm
 from flask_wtf.file import FileField
-from werkzeug.utils import secure_filename
 
 
 class FileForm(FlaskForm):
-    file = FileField()
-
-    def upload_file(self) -> bool:
-
-        file = self.file.data
-        filename = secure_filename(file.filename)
-        files = {'file': (filename, file)}
-
-
-        return True
+    file = FileField()    
--- service/views/index.py
+++ service/views/index.py
@@ -1,14 +1,26 @@
-from flask import Blueprint, render_template
+from pathlib import Path
 
+from flask import Blueprint, redirect, render_template, request, url_for
+
+from service import config
 from service.forms.upload import FileForm
 
+app_config = config.load_from_env()
 view = Blueprint('index', __name__)
+upload_dir = Path(app_config.upload_dir)
 
 
 @view.route('/', methods=['GET', 'POST'])
 def index():
-    form = FileForm(meta={'csrf': False})
     title = 'Поиск аномалий'
+
+    form = FileForm(meta={'csrf': False})
+    if form.validate_on_submit():
+        file = request.files['file']
+        upload_file = upload_dir / str(file.filename)
+        file.save(upload_file)
+        return redirect(url_for('index.index'))
+
     return render_template(
         'index.html',
         page_title=title,
