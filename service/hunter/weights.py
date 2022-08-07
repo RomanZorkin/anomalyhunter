41350ad3df06d27dbcc229027b8f57bf3b5d9464
RomanZorkin <zorkin.roman@gmail.com>
2022-08-07 21:11:35 +0700

    feature: insert timer

--- service/hunter/weights.py
+++ service/hunter/weights.py
@@ -1,4 +1,5 @@
 import difflib as dif
+from time import time
 import logging
 from pathlib import Path
 
@@ -14,6 +15,9 @@ weights_dir = app_config.path.weights_dir
 word_depth = 6
 precision = 0.5
 row_counter = 0
+col_counter = 0
+grade = 100
+level = 1
 
 
 def make_short(name, **kwargs):
@@ -29,13 +33,19 @@ def make_pattern(frame: pd.DataFrame, word_count: int) -> pd.DataFrame:
 
 
 def compare_base(col_word, col_num, **kwargs):
-    global row_counter
-    row_counter = kwargs['counter']
+    global row_counter, level  # noqa: WPS420 need for compare_base function work
+    row_counter = kwargs['counter']  # noqa: WPS442 use only for compare_base function work
     words_in_row: pd.DataFrame = kwargs['row_frame']
     row_word = words_in_row.iloc[row_counter, col_num]
-    t = dif.SequenceMatcher(None, col_word, row_word).ratio()
+    weight = dif.SequenceMatcher(None, col_word, row_word).ratio()
+    #logger.debug(col_num)
+
+    if level == col_num // grade:
+        level += 1
+        logger.debug(f'{col_num}, {level}, {col_num // grade}')
+
     row_counter += 1
-    return t
+    return weight
 
 
 def compare_ind(col_frame: pd.DataFrame, **kwargs):
@@ -48,6 +58,7 @@ def create_weights(
     asset_name: str = 'основное средство',
     ind: str = 'ind',
 ) -> bool:
+    start = time()
     excel_weights = weights_dir / excel_file.name
     frame = pd.read_excel(excel_file, index_col=ind)
     frame['short'] = frame[asset_name].apply(make_short, depth=word_depth)
@@ -57,10 +68,14 @@ def create_weights(
     words_in_row = pattern.copy()
     words_in_col = pattern.copy()
 
-    words_in_row.iloc[:word_count] = frame['short']
+    words_in_row.iloc[:word_count] = frame['short']  # noqa: WPS362 pandas style
     words_in_col = words_in_row.T
     weights: pd.DataFrame = words_in_col.apply(compare_ind, row_frame=words_in_row)
+    frame_time = time() - start
+    global row_counter, level
+    row_counter, level = 0, 0
     weights.astype('float')
     weights[weights[:] > precision].to_excel(excel_weights)
-
+    excel_time = time() - start
+    logger.debug(f'frame time - {frame_time};/nexcel time - {excel_time}')
     return True
